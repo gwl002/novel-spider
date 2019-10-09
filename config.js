fd72c750e593d9d8b2ef884f3d2581b4e236e492
Devin Gong <devingong@cherrypicks.com>
2019-10-09 18:27:35 +0800

    add socket test

--- config.js
+++ config.js
@@ -6,5 +6,11 @@ module.exports = {
 	  	host:"149.28.149.49",
 	  	password:"gwl002.tk",
 	  	db:0
+	},
+	goEasy:{
+		subKey: "BS-2be3e03d374340849a09e6b44308a503",
+		pubKey: "BC-09b2c974fff24e639d52c2e849d47d44",
+		cdnHost: "cdn-hangzhou.goeasy.io",
+		restHost: "rest-hangzhou.goeasy.io",
 	}
 }
---
+++ socket/index.js
@@ -0,0 +1,29 @@
+var rp = require('../crawler/node_modules/request-promise-native');
+
+var config = require('../config.js');
+
+async function publishNotification(channel,content){
+	try{
+		let result = await rp({
+			method:"POST",
+			uri: "http://" + config.goEasy.restHost + "/publish",
+			form:{
+				appKey: config.goEasy.pubKey,
+				channel,
+				content
+			}
+		})
+		return result;
+	}catch(err){
+		console.log(err);
+		throw err;
+	}
+	
+	
+}
+
+
+publishNotification("test","hello")
+
+
+// module.exports = publishNotification
